90073c6e892ae2be9982e8cb202d012f2b0e1721
yuk2000 <1461124346@qq.com>
2023-08-27 18:50:07 +0800

    第八天和第七天

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/service/impl/PasswordAuthServiceImpl.java
@@ -0,0 +1,70 @@
+package com.xuecheng.ucenter.service.impl;
+
+import com.alibaba.nacos.api.utils.StringUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.auth.feignclient.CheckCodeClient;
+import com.xuecheng.ucenter.mapper.XcUserMapper;
+import com.xuecheng.ucenter.model.dto.AuthParamsDto;
+import com.xuecheng.ucenter.model.dto.XcUserExt;
+import com.xuecheng.ucenter.model.po.XcUser;
+import com.xuecheng.ucenter.service.AuthService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service("password_authservice")
+public class PasswordAuthServiceImpl implements AuthService {
+
+    @Autowired
+    XcUserMapper xcUserMapper;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    CheckCodeClient checkCodeClient;
+
+
+    @Override
+    public XcUserExt execute(AuthParamsDto authParamsDto) {
+
+        //账号
+        String username = authParamsDto.getUsername();
+
+        //校验验证码
+        String checkcode = authParamsDto.getCheckcode();
+        String checkcodekey = authParamsDto.getCheckcodekey();
+
+        if(StringUtils.isBlank(checkcodekey) || StringUtils.isBlank(checkcode)){
+            throw new RuntimeException("验证码为空");
+
+        }
+        Boolean verify = checkCodeClient.verify(checkcodekey, checkcode);
+        if(!verify){
+            throw new RuntimeException("验证码输入错误");
+        }
+
+
+
+
+
+        XcUser user = xcUserMapper.selectOne(new LambdaQueryWrapper<XcUser>().eq(XcUser::getUsername, username));
+        if(user==null){
+            //返回空表示用户不存在
+            throw new RuntimeException("账号不存在");
+        }
+        XcUserExt xcUserExt = new XcUserExt();
+        BeanUtils.copyProperties(user,xcUserExt);
+        //校验密码
+        //取出数据库存储的正确密码
+        String passwordDb  =user.getPassword();
+        String passwordForm = authParamsDto.getPassword();
+        boolean matches = passwordEncoder.matches(passwordForm, passwordDb);
+        if(!matches){
+            throw new RuntimeException("账号或密码错误");
+        }
+        return xcUserExt;
+    }
+}
+
---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/feignclient/MediaServiceClient.java
@@ -0,0 +1,17 @@
+package com.xuecheng.content.feignclient;
+
+import com.xuecheng.content.config.MultipartSupportConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+@FeignClient(value = "media-api", configuration = {MultipartSupportConfig.class},fallbackFactory = MediaServiceClientFallbackFactory.class)
+public interface MediaServiceClient {
+
+    @RequestMapping(value = "/media/upload/coursefile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    String uploadFile(@RequestPart("filedata") MultipartFile upload, @RequestParam(value = "objectName",required=false) String objectName);
+
+}
